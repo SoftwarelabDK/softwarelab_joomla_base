a16b16db6af306d2770da5dc99d45759df009c62
Terning <kgordont@hotmail.com>
2014-10-14 23:21:41 +0200

    Default Menu and design
    Simple responsive menu, and Softwarelab branding.

--- index.php
+++ index.php
@@ -41,21 +41,79 @@ else
 // Add current user information
 $user = JFactory::getUser();
 
-
 ?>
 <!DOCTYPE html>
 <html xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
-
-
+<!--
+    ##########################################
+    ## Softwarelab ApS                      ##
+    ## 5000 Odense C                        ##
+    ## Danmark                              ##
+    ## software@softwarelab.dk              ##
+    ## http://www.softwarelab.dk            ##
+    ## Designer: Softwarelab                ##
+    ## Dato: November 2014                  ##
+    ## Version: 1.0                         ##
+    ##########################################
+-->
+<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet/less" type="text/css" href="templates/softwarelab_joomla_base/less/template.less" />
 </head>
 <body>
-<jdoc:include type="modules" name="position-2" /> 
-<jdoc:include type="component" />
-<jdoc:include type="modules" name="bottom" />
-
-<link rel="stylesheet/less" type="text/css" href="templates/softwarelab_joomla_base/less/softwarelab.less" />
-
-<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js" type="text/javascript"></script>
+<!-- Direct Template Image -->
+<div class="col-md-12 logo">
+	<a href="index.php"><img src="<?php echo $this->baseurl; ?>/templates/softwarelab_joomla_base/images/logo.png" alt="Custom image" class="customImage" /></a>
+</div>
+<!-- Default Boostrap Menu  -->
+<div class="col-md-12 softmenu">
+	<nav class="navbar navbar-default" role="navigation">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#topnav">
+				<span class="sr-only"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+		<div class="collapse navbar-collapse" id="topnav">
+			<jdoc:include type="modules" name="softmenu" />  <!-- Put your menu position to 'softmenu' -->
+		</div>
+	</nav>
+</div>
+<!-- Costum HTML -->
+<div class="col-md-12 swl">
+	<h1>THANK YOU FOR USING <br/><b>Softwarelab Joomla Base Template.</b></h1>
+	<h2>Feel free to fork at <a href="https://github.com/SoftwarelabDK/softwarelab_joomla_base"><i class="fa fa-github">Github</i></a></h2>
+	<h3>Or visit us at <a href="http//www.softwarelab.dk">www.softwarelab.dk</a></h3>
+</div>
+<!-- Article Container -->
+<div class="article_wrapper">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<jdoc:include type="message" />
+				<jdoc:include type="component" />
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Example Costum Module -->
+<?php if ($this->countModules('modulename')) : ?>
+	<div class="module_wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<w:module name="modulename" />
+				</div>
+			</div>
+		</div>
+	</div>
+<?php endif; ?>
 </body>
+<!-- ADD scripts here -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/1.7.5/less.min.js" type="text/javascript"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </html>
